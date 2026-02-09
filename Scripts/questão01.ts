564060bc1a5674b39ef5704661eb598ec0871735
Davi Rodrigues de Mello <davirmelo16@gmail.com>
2026-02-09 19:13:49 -0300

    feat: conclusão da questão 1

--- Scripts/questão01.ts
+++ Scripts/questão01.ts
@@ -7,8 +7,9 @@ const resultado = adicionar(20, 30); / / resultado = 5 */
 
 let variavelTotal = 100;
 
-function adicionar (a, b){
+function adicionar (a: number, b: number) {
     return  a + b;
 }
 
 const resultado = adicionar(20, 30);
+console.log(resultado);
--- questão01.d.ts
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=quest%C3%A3o01.d.ts.map
